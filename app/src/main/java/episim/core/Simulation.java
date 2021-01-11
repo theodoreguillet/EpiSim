b54dc483ee7549f15732cf1d1da34a77c043f3c6
Watchtux <guillettheodore@gmx.com>
2021-01-11 23:57:38 +0100

    simulation thread

--- app/src/main/java/episim/core/Simulation.java
+++ app/src/main/java/episim/core/Simulation.java
@@ -1,28 +1,106 @@
 package episim.core;
 
+import java.util.Timer;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * La simulation de l'épidémie
  */
 public class Simulation {
     private SimulationConfig config;
-    private SimulationState state;
-    private int speed; // Vitesse de la simulation en jours par seconde
-    boolean paused;
-    boolean started;
-    private Thread thread;
+    private double speed = 1; // Vitesse de la simulation en jours par seconde
+
+    private ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);;
+    private AtomicReference<SimulationState> state = new AtomicReference<>(null);
+    private boolean started = false;
+    private boolean paused = false;
 
     public Simulation(SimulationConfig config) {
         this.config = (SimulationConfig)config.clone();
-        this.speed = 1;
-        this.paused = false;
     }
 
-    public void start() {
+    public synchronized void start() {
+        if(!started) {
+            startExecutor();
+            started = true;
+        }
     }
 
-    public void stop() {
-
+    public synchronized void stop() throws RuntimeException {
+        if(started) {
+            stopExecutor();
+            clear();
+        }
     }
 
+    public synchronized void pause() {
+        if(started && !paused) {
+            stopExecutor();
+            paused = true;
+        }
+    }
 
+    public synchronized void resume() {
+        if(started && paused) {
+            startExecutor();
+            paused = false;
+        }
+    }
+
+    public synchronized boolean isStarted() {
+        return started;
+    }
+
+    public synchronized boolean isPaused() {
+        return paused;
+    }
+
+    public synchronized void setSpeed(double speed) {
+        this.speed = speed;
+        executor.shutdown();
+        startExecutor();
+    }
+
+    public synchronized double getSpeed() {
+        return speed;
+    }
+
+    public SimulationState getState() {
+        return state.get();
+    }
+
+    private void clear() {
+        started = false;
+        paused = false;
+        state.set(null);
+    }
+
+    private void startExecutor() {
+        int period = Math.max((int)(1000.0 / speed), 10);
+        executor.scheduleAtFixedRate(() -> {
+            // Do stuff
+        }, 0, period, TimeUnit.MILLISECONDS);
+    }
+
+    private void stopExecutor() throws RuntimeException {
+        if(!started) {
+            return;
+        }
+        executor.shutdownNow();
+        try {
+            if(!executor.awaitTermination(10, TimeUnit.SECONDS)) {
+                // TODO: throw custom exception
+                throw new RuntimeException("Thread does not terminate");
+            }
+        } catch (InterruptedException err) {
+            err.printStackTrace(System.err);
+            Thread.currentThread().interrupt();
+        }
+        started = false;
+    }
 }
